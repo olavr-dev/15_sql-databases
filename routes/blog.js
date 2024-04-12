e9056c717cef967c0e5f57bb305882b0f5feee50
Olav Rorvik <olavr.dev@gmail.com>
2024-04-12 10:31:11 +0200

    added edit and delete post logic

--- routes/blog.js
+++ routes/blog.js
@@ -45,9 +45,52 @@ router.get('/posts/:id', async function (req, res) {
     return res.status(404).render('404');
   }
 
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    }),
+  };
+
   res.render('post-detail', {
-    post: posts[0],
+    post: postData,
   });
+});
+
+router.get('/posts/:id/edit', async function (req, res) {
+  const query = `
+  SELECT * FROM posts WHERE id = ?
+  `;
+  const [posts] = await db.query(query, [req.params.id]);
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+
+  res.render('update-post', { post: posts[0] });
+});
+
+router.post('/posts/:id/edit', async function (req, res) {
+  const query = `
+  UPDATE posts SET title = ?, summary = ?, body = ?
+  WHERE id = ?
+  `;
+  await db.query(query, [req.body.title, req.body.summary, req.body.content, req.params.id]);
+
+  res.redirect('/posts');
+});
+
+router.post('/posts/:id/delete', async function (req, res) {
+  const query = `
+  DELETE FROM posts WHERE id = ?
+  `;
+
+  await db.query(query, [req.params.id]);
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
